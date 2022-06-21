8e1f9be818a9d961e540ef6ad7bf720762e45a57
sergiopani <xpanisergio@gmail.com>
2022-06-21 10:01:33 +0200

    Final Curso

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <h1>Vue JS</h1>
-  <CarComposition />
-  <hr/>
-  <CarOptions />
+  <Car :power="power"/>
 </template>
 
 <script>
-import CarComposition from "./components/CarComposition";
-import CarOptions from "./components/CarOptions";
+import Car from "./components/Car";
 export default {
   components: {
-    CarComposition,
-    CarOptions,
+    Car,
+  },
+  setup() {
+    let power = 30;
+    return {
+      power,
+    };
   },
 };
 </script>
---
+++ src/components/Car.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Coche - Composition Api</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+</template>
+
+<script>
+
+export default {
+  props:{
+    power: {
+      type: Number,
+      default: 60,
+    },
+
+  },
+  setup() {
+    const brand = "Audi";
+    const model = "A4";
+
+    return {
+      brand,
+      model,
+    };
+  },
+};
+</script>
+
+<style></style>
